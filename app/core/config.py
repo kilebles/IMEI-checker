18f25b8f820f272e5c6c8ad7568e2e4cb60c558d
Elisey <eliseyblajen@gmail.com>
2025-02-02 19:57:22 +0500

    Initial structure

---
+++ app/core/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+  BOT_TOKEN = os.getenv("BOT_TOKEN")
+  HOST = os.getenv("APP_HOST", "0.0.0.0")
+  PORT = int(os.getenv("APP_PORT", "8080"))
+  WEBHOOK_HOST = os.getenv("WEBHOOK_HOST")
+  WEBHOOK_PATH = os.getenv("WEBHOOK_PATH", "/webhook")
+  WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+  
+config = Config()
---
+++ run.py
@@ -0,0 +1,40 @@
+import logging
+import uvicorn
+from fastapi import FastAPI, Request
+from contextlib import asynccontextmanager
+from aiogram import Bot, Dispatcher
+from aiogram.types import Update
+from app.core.config import config
+from app.commands.set_commands import set_commands
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=config.BOT_TOKEN)
+dp = Dispatcher()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+  logging.info("Starting app...")
+  await bot.set_webhook(config.WEBHOOK_URL)
+  await set_commands(bot)
+
+  yield
+
+  await bot.delete_webhook()
+  await bot.session.close()
+
+app = FastAPI(lifespan=lifespan)
+
+@app.post(config.WEBHOOK_PATH)
+async def process_webhook(request: Request):
+  json_data = await request.json()
+  update = Update(**json_data)
+  await dp.feed_update(bot, update)
+  return {"status": "ok"}
+
+if __name__ == "__main__":
+  uvicorn.run(
+    app, 
+    host=config.HOST, 
+    port=config.PORT
+  )
